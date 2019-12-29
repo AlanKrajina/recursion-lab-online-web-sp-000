d8f6cc7385cfd5b95c316be6ddfd7b1fd3fda188
Alan Krajina <alankrajina88@gmail.com>
2019-12-29 12:54:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function printString(string){
 
   if (string.length > 1){
 
-    let mySubString = myString.substring(1, myString.length);
+    let mySubString = string.substring(1, myString.length);
     printString(mySubString);
     } else {
     return true;
