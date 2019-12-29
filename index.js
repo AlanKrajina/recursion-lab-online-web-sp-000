15cd1c34f93efd145b996eab2dad1ec534f1afcb
Alan Krajina <alankrajina88@gmail.com>
2019-12-29 12:54:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solution here!
 function printString(string){
-  
+
   if (string.length > 1){
-    
+
     let mySubString = myString.substring(1, myString.length);
     printString(mySubString);
     } else {
